f2380390d1837f614f469faed6e436a97308d807
amplifyaccess <amplifyaccess@zohomail.com.au>
2026-04-09 02:05:50 +1000

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-
-function TestApp() {
-  return (
-    <div style={{ padding: 40, fontFamily: 'sans-serif' }}>
-      <h1>Amplify Access test</h1>
-      <p>If you can see this, React is working and the problem is inside App.jsx.</p>
-    </div>
-  )
-}
+import App from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <TestApp />
+    <App />
   </React.StrictMode>,
 )
